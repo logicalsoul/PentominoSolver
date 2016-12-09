8a9e957656705e9d9c04c60d6294aa64fd6321a5
logicalsoul <logical.soul@gmail.com>
2016-12-09 16:08:05 +0900

    add time measure

--- PenitominoSolver/main.cpp
+++ PenitominoSolver/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <time.h>
 #include "PentominoSolver.h"
 
 int main()
 {
 	PentominoSolver solver(10, 6);
+	int n;
 
-	std::cout << solver.Solve() << std::endl;
+	clock_t t = clock();
+	n = solver.Solve();
+	t = clock() - t;
+
+	std::cout << n << " solutions" <<  std::endl;
+	std::cout << "time:" << (double)t / CLOCKS_PER_SEC << " sec." << std::endl;
 	system("pause");
 
 	return 0;
